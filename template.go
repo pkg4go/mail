9160256dcf21534194621982453f6740a4ffb68b
haoxin <coderhaoxin@outlook.com>
2016-09-30 14:11:09 +0800

    typo

--- template.go
+++ template.go
@@ -1,6 +1,6 @@
 package mail
 
-func fmtMsg(title, content, to string) []byte {
+func FmtMsg(title, content, to string) []byte {
 	return []byte("To: " + to + "\r\n" +
 		"Subject: " + title + "\r\n" +
 		"\r\n" + content + "\r\n")
--- template_test.go
+++ template_test.go
@@ -6,7 +6,7 @@ import "testing"
 func TestFmtMsg(t *testing.T) {
 	a := A{t}
 
-	msg := fmtMsg("Hi", "hello world!", "test@mail.com")
+	msg := FmtMsg("Hi", "hello world!", "test@mail.com")
 	result := "To: test@mail.com\r\n" +
 		"Subject: Hi\r\n" +
 		"\r\nhello world!\r\n"
